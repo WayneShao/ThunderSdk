933a4e866c730d1663f7b45648835cf20e327c7e
ShaoWei <826769314@qq.com>
2018-03-07 23:03:46 +0800

    初始化

--- ThunderSdk/Properties/AssemblyInfo.cs
+++ ThunderSdk/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("XunleiTest")]
+[assembly: AssemblyTitle("XLDownload")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XunleiTest")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Guangdong Medical University")]
+[assembly: AssemblyProduct("XLDownload")]
+[assembly: AssemblyCopyright("Copyright © Guangdong Medical University 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("e3ff733b-b5a0-4ae3-b454-37c97154ef33")]
+[assembly: Guid("6cdc89b8-7708-490f-a0fe-80fccf703288")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      修订号
 //
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
-// 方法是按如下所示使用“*”: :
+//通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ThunderSdkDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ThunderSdk;
+
+namespace ThunderSdkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new DownloadManager(1, @"D:\");
+            manager.CreateNewTask("https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=770549720,375505130&fm=173&s=612A66F94AA394CE4A84E71B030050D7&w=218&h=146&img.JPEG",
+                "2018-1-30-5ZD6R0BZZ.rar");
+
+            manager.TaskDownload += (s, e) =>
+            {
+                if (!(s is DownFileInfo info)) return;
+
+                Console.WriteLine(info.TaskInfo.Percent);
+            };
+
+            manager.TaskCompleted += (s, e) =>
+              {
+                  if (!(s is DownFileInfo info)) return;
+
+                  Console.WriteLine(info.FileName + "下载完成");
+              };
+
+            manager.StartAllTask();
+
+            Console.ReadKey();
+        }
+    }
+}
